5f40b7434142b43a3edec3d1dc5b18179d5708b3
gaoqinqinshan <1626733251@qq.com>
2023-10-11 00:02:27 +0800

    分页

---
+++ src/main/java/org/example/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package org.example.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 利用mybatisplus的分页插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    //分页插件
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
