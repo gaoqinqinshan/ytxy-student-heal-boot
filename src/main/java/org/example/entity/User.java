bd176130f818b9a7f7c4bd5cb2f2a520307e08a5
gaoqinqinshan <1626733251@qq.com>
2023-10-11 17:50:12 +0800

    user对象实例化

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,115 @@
+package org.example.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户实例化
+ */
+@Data
+@Accessors(chain = true)
+@ApiModel(value = "User对象", description = "")
+@EqualsAndHashCode(callSuper = false)
+public class User implements Serializable {
+
+    //定义程序序列化ID
+    private static final long serialVersionUID = 1;
+
+    //Mybatis-plus用来实现自增的(id 需要自增)
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    //创建时间
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDatetime;
+    //更新时间
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateDatetime;
+
+    /**
+     * 出生
+     */
+    private Date birth;
+
+    /**
+     * 大学
+     */
+    private String college;
+
+    /**
+     * 年级
+     */
+    private String grade;
+
+    /**
+     * 主要
+     */
+    private String major;
+
+
+    /**
+     * name password
+     */
+    private String name;
+    private String password;
+
+    /**
+     * 角色的id 性别
+     */
+    private Integer roleId;
+    private String sex;
+    private String stuNo;
+    private String username;
+
+    /**
+     * 文化水平
+     */
+    private String cultureLevel;
+
+    /**
+     * 国家
+     */
+    private String nation;
+
+    /**
+     * 籍贯
+     */
+    private String nativePlace;
+
+    /**
+     * 职业
+     */
+    private String occupation;
+
+    /**
+     * 目前居住地
+     */
+    private String presentAddress;
+
+    /**
+     * 工作的地方
+     */
+    private String workPlace;
+
+    /**
+     * 照片
+     */
+    private String photo;
+
+    /**
+     * 真实年龄
+     */
+    private String realAge;
+
+    /**
+     * 老师的id
+     */
+    private String teacherId;
+}
